a48eeef1d210af07b20c354b3b222278770aebfb
machard <matthieu__@hotmail.fr>
2020-11-02 12:13:07 +0100

    comments

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ const {
   getAccountBridge,
 } = require("@ledgerhq/live-common/lib/bridge");
 const { withDevice } = require("@ledgerhq/live-common/lib/hw/deviceAccess");
+
+// see https://github.com/machard/ledger-live-common/blob/LL-3715/src/families/ethereum/hw-signMessage.js#L109
 const signMessage = require("@ledgerhq/live-common/lib/hw/signMessage").default;
 
 const wcUtilities = require("walletconnect-example-dapp/src/helpers/utilities.ts");
-console.log(wcUtilities);
+const { eip712 } = require("walletconnect-example-dapp/src/helpers/eip712.ts");
 
 const currencyId = "ethereum";
 const currency = getCryptoCurrencyById(currencyId);
@@ -90,6 +92,8 @@ const main = async() => {
     derivationMode: account.derivationMode,
   };
 
+  console.log(message);
+
   await timeout(1000);
 
   result = await withDevice(deviceId)((t) =>
@@ -99,6 +103,7 @@ const main = async() => {
     .toPromise();
   result = result.signature;
 
+  // see https://github.com/WalletConnect/walletconnect-example-dapp/blob/61f1ea192cd5060c37ff876972058b7d4c41303b/src/App.tsx#L359
   hash = wcUtilities.hashPersonalMessage(messageData);
   valid = await wcUtilities.verifySignature(account.freshAddress, result, hash, account.currency.ethereumLikeInfo.chainId);
 
@@ -106,82 +111,7 @@ const main = async() => {
 
   console.log("----testing typedmessage");
 
-  messageData = {
-    "types": {
-      "EIP712Domain": [{
-        "name": "name",
-        "type": "string"
-      }, {
-        "name": "version",
-        "type": "string"
-      }, {
-        "name": "verifyingContract",
-        "type": "address"
-      }],
-      "RelayRequest": [{
-        "name": "target",
-        "type": "address"
-      }, {
-        "name": "encodedFunction",
-        "type": "bytes"
-      }, {
-        "name": "gasData",
-        "type": "GasData"
-      }, {
-        "name": "relayData",
-        "type": "RelayData"
-      }],
-      "GasData": [{
-        "name": "gasLimit",
-        "type": "uint256"
-      }, {
-        "name": "gasPrice",
-        "type": "uint256"
-      }, {
-        "name": "pctRelayFee",
-        "type": "uint256"
-      }, {
-        "name": "baseRelayFee",
-        "type": "uint256"
-      }],
-      "RelayData": [{
-        "name": "senderAddress",
-        "type": "address"
-      }, {
-        "name": "senderNonce",
-        "type": "uint256"
-      }, {
-        "name": "relayWorker",
-        "type": "address"
-      }, {
-        "name": "paymaster",
-        "type": "address"
-      }]
-    },
-    "domain": {
-      "name": "GSN Relayed Transaction",
-      "version": "1",
-      "chainId": 42,
-      "verifyingContract": "0x6453D37248Ab2C16eBd1A8f782a2CBC65860E60B"
-    },
-    "primaryType": "RelayRequest",
-    "message": {
-      "target": "0x9cf40ef3d1622efe270fe6fe720585b4be4eeeff",
-      "encodedFunction": "0xa9059cbb0000000000000000000000002e0d94754b348d208d64d52d78bcd443afa9fa520000000000000000000000000000000000000000000000000000000000000007",
-      "gasData": {
-        "gasLimit": "39507",
-        "gasPrice": "1700000000",
-        "pctRelayFee": "70",
-        "baseRelayFee": "0"
-      },
-      "relayData": {
-        "senderAddress": "0x22d491bde2303f2f43325b2108d26f1eaba1e32b",
-        "senderNonce": "3",
-        "relayWorker": "0x3baee457ad824c94bd3953183d725847d023a2cf",
-        "paymaster": "0x957F270d45e9Ceca5c5af2b49f1b5dC1Abb0421c"
-      }
-    }
-  };
+  messageData = eip712.example;
 
   message = {
     path: account.freshAddressPath,
@@ -190,6 +120,8 @@ const main = async() => {
     derivationMode: account.derivationMode,
   };
 
+  console.log(message);
+
   await timeout(1000);
 
   result = await withDevice(deviceId)((t) =>
@@ -199,6 +131,7 @@ const main = async() => {
     .toPromise();
   result = result.signature;
 
+  // see https://github.com/WalletConnect/walletconnect-example-dapp/blob/61f1ea192cd5060c37ff876972058b7d4c41303b/src/App.tsx#L409
   hash = wcUtilities.hashTypedDataMessage(JSON.stringify(messageData));
   valid = await wcUtilities.verifySignature(account.freshAddress, result, hash, account.currency.ethereumLikeInfo.chainId);
 
